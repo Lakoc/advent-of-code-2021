2a4d20bbc69c4bbefc9519c0d05b79e5e855888b
Lakoc <xpolok03@stud.fit.vutbr.cz>
2021-12-06 13:27:35 +0100

    simplified version

--- days/day_06/day.py
+++ days/day_06/day.py
@@ -1,50 +1,31 @@
 from days.base import Day
 from operator import add
 from functools import reduce
+import numpy as np
 
 
 class Day06(Day):
 
     def __init__(self):
         super().__init__('days/day_06/input.txt')
-        self.days = 0
-        self.lanternfishes = self.input_lines[0].split(',')
-        self.initialize_population()
+        self.lanternfishes = list(map(int, self.input_lines[0].split(',')))
 
-    def initialize_population(self):
-        self.fishes_count = {i: 0 for i in range(9)}
-        for lanternfish in self.lanternfishes:
-            self.fishes_count[int(lanternfish)] += 1
+        fish_age_classes = 9
 
-    def simulate_life_cycle(self, days):
-        days_pop = [reduce(add, self.fishes_count.values())]
-        for _ in range(days):
-            new_fishes_count = {i: 0 for i in range(9)}
-            for fish_age, count in self.fishes_count.items():
-                if fish_age == 0:
-                    new_fishes_count[6] += count
-                    new_fishes_count[8] += count
-                    new_fishes_count[0] = 0
-                else:
-                    new_fishes_count[fish_age - 1] += count
+        self.population_gen_mat = np.eye(fish_age_classes, k=1, dtype=int)
+        self.population_gen_mat[(6, 8), (0, 0)] = 1
 
-            self.fishes_count = new_fishes_count
-            population = reduce(add, self.fishes_count.values())
-            days_pop.append(population)
-
-        return days_pop
+    def simulate_lifecycle(self, days):
+        lifecycle_matrix = np.linalg.matrix_power(self.population_gen_mat, days)
+        fishes_generate_classes = lifecycle_matrix.sum(axis=0)
+        population = reduce(add, [fishes_generate_classes[fish] for fish in self.lanternfishes])
+        return population
 
     def part_one(self):
-        self.days = 80
-        population_per_day = self.simulate_life_cycle(self.days)
-
-        return population_per_day[-1]
+        return self.simulate_lifecycle(80)
 
     def part_two(self):
-        self.days = 256
-        self.initialize_population()
-        population_per_day = self.simulate_life_cycle(self.days)
-        return population_per_day[-1]
+        return self.simulate_lifecycle(256)
 
 
 if __name__ == '__main__':
