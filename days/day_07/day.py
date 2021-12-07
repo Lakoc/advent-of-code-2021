bc645bdaa964cf7d6601a5146315fc03945c06a0
Lakoc <xpolok03@stud.fit.vutbr.cz>
2021-12-07 10:08:35 +0100

    day 7 fixes

--- days/day_07/day.py
+++ days/day_07/day.py
@@ -7,22 +7,25 @@ class Day07(Day):
 
     def __init__(self):
         super().__init__('days/day_07/input.txt')
-        self.crab_positions = np.array(list(map(int, self.input_lines[0].split(','))))
+        self.crab_positions = np.array(self.input_lines[0].split(','), dtype=int)
+
+    def calculate_distance(self, value):
+        return np.abs(self.crab_positions - np.ones_like(self.crab_positions) * value)
+
+    def get_min_max_vals(self):
+        return np.min(self.crab_positions), np.max(self.crab_positions)
 
     def part_one(self):
-        min_val = np.min(self.crab_positions)
-        max_val = np.max(self.crab_positions)
-        fuel_consumptions = [np.sum(np.abs(self.crab_positions - np.ones_like(self.crab_positions) * val)) for val in
+        min_val, max_val = self.get_min_max_vals()
+        fuel_consumptions = [np.sum(self.calculate_distance(val)) for val in
                              range(min_val, max_val + 1)]
         return min(fuel_consumptions)
 
     def part_two(self):
-        min_val = np.min(self.crab_positions)
-        max_val = np.max(self.crab_positions)
+        min_val, max_val = self.get_min_max_vals()
         fuel_consumptions = [np.sum(value / 2 * (1 + value)) for value in
-                             (np.abs(self.crab_positions - np.ones_like(self.crab_positions) * val) for val in
+                             (self.calculate_distance(val) for val in
                               range(min_val, max_val + 1))]
-
         return int(min(fuel_consumptions))
 
 
